6056a10380b8532f5f7bc447b40a898a15b36602
Ikhsan Abdul Latif <126028020+mrsan83@users.noreply.github.com>
2023-02-24 09:33:38 +0700

    Create 01_input-output1.cpp

---
+++ 02_Deklarasi C++/01_input-output1.cpp
@@ -0,0 +1,11 @@
+//Cara deklarasi 1
+
+#include <stdio.h>
+
+int main(){
+  int a;
+  
+  printf("Input sebuah bilangan bulat: ");  //Program menampilkan (output) kalimat Input sebuah bilangan bulat: di layar
+  scanf("%d", a);                           //Program menampung bilangan yang diinput di dalam variabel a
+  
+}
