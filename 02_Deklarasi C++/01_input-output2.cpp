1484a340850f926e0e6b3c7bdd342906e14f9512
Ikhsan Abdul Latif <126028020+mrsan83@users.noreply.github.com>
2023-02-24 09:27:29 +0700

    Create 01_input-output2.cpp

---
+++ 02_Deklarasi C++/01_input-output2.cpp
@@ -0,0 +1,11 @@
+//Cara deklarasi 2
+
+#include <iostream>                           //Input std::cin, Output std::cout
+
+int main(){
+  int a;
+  
+  std::cout<<"Input sebuah bilangan bulat: ";  //Program menampilkan (output) kalimat Input sebuah bilangan bulat: di layar
+  std::cin>>a;                                 //Program menampung bilangan yang diinput di dalam variabel a
+  
+}
